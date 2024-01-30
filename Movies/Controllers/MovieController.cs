265778f0978c01e9759d27cd11ce6c04649a72b0
Antoine Lardant <antoine.lardant@gmail.com>
2024-01-30 15:12:19 +0100

    end Movie CRUD

--- Movies/Controllers/MovieController.cs
+++ Movies/Controllers/MovieController.cs
@@ -4,6 +4,8 @@ using MovieMaker.Models;
 using Movies.Data;
 using Movies.DTO;
 using Movies.Repository;
+using Serilog.Filters;
+using System.Globalization;
 using System.Security.Claims;
 
 namespace Movies.Controllers
@@ -13,40 +15,45 @@ namespace Movies.Controllers
         private readonly MovieRepository _movieRepository;
         private readonly UserRepository _userRepository;
         private readonly ILogger<MovieController> _logger;
-        public MovieController(MovieRepository movieRepository,UserRepository userRepository, ILogger<MovieController> logger)
+        public MovieController(MovieRepository movieRepository, UserRepository userRepository, ILogger<MovieController> logger)
         {
             _movieRepository = movieRepository;
             _logger = logger;
             _userRepository = userRepository;
         }
 
-        [HttpGet]
+        [HttpGet("Reviews")]
         public async Task<IActionResult> GetAllReviews()
         {
             try
             {
                 var movies = await _movieRepository.GetAllMoviesAsync();
-                var movieDtos = movies.Select(movie => ConvertToMovieDto(movie)).ToList();
-                return Ok(movies);
+                var moviesDto = new List<MovieDto>();
+                foreach (Movie movie in movies)
+                {
+                    var movieDto = ConvertToMovieDto(movie);
+                    moviesDto.Add(movieDto);
+                }
+                return Ok(moviesDto);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"An error occurred during retrieving data: {ex.Message}");
-                return StatusCode(500, "An error occurred while fetching reviews.");
+                return StatusCode(500, "An error occurred while fetching movies.");
             }
 
         }
 
-        [HttpGet("id")]
+        [HttpGet("Review/{id}")]
         public async Task<IActionResult> GetMovieById(int id)
         {
             try
             {
-                var movie = await _movieRepository.GetMoviesByIdAsync(id);
+                var movie = await _movieRepository.GetMovieByIdAsync(id);
                 if (movie == null)
                 {
                     _logger.LogError($"Movie with ID {id} not found.");
-                    return NotFound($"Movie with ID {id} not found.");
+                    return NotFound($"An error occurred while fetching the movie.");
                 }
                 var movieDto = ConvertToMovieDto(movie);
                 return Ok(movieDto);
@@ -54,7 +61,154 @@ namespace Movies.Controllers
             catch (Exception ex)
             {
                 _logger.LogError($"An error occurred during retrieving data: {ex.Message}");
-                return StatusCode(500, "An error occurred while fetching the review.");
+                return StatusCode(500, "An error occurred while fetching the movie.");
+            }
+        }
+
+        [Authorize]
+        [HttpPost("CreateReview")]
+        public async Task<IActionResult> CreateMovie(MovieDto movieDto)
+        {
+            // Get the currently logged-in user's info
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var loggedUser = await _userRepository.GetUserByIdAsync(userId);
+
+            if (userId == null || loggedUser == null)
+            {
+                return NotFound("Échec de la création du commentaire.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Échec de la création du commentaire.");
+            }
+
+            try
+            {
+                var movie = new Movie
+                {
+                    Id = movieDto.Id,
+                    Title = movieDto.Title,
+                    Description = movieDto.Description,
+                    Author = movieDto.Author,
+                    Genre = Enum.Parse<MovieGenre>(movieDto.Genre), // parse the value to convert them to appropriate types
+                    DateOfRelease = DateTime.ParseExact(movieDto.DateOfRelease, "dd-MM-yyyy", CultureInfo.InvariantCulture), // parse the value to convert them to appropriate types
+                    UserId = userId,
+                    User = loggedUser
+                };
+
+                var isMovieCreated = await _movieRepository.CreateMovieAsync(movie);
+
+                if (!isMovieCreated)
+                {
+                    _logger.LogError($"An error occurred during creating data");
+                    return StatusCode(500, "An error occurred while creating the movie.");
+                }
+
+                return Ok(movie);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred during creating data: {ex.Message}");
+                return StatusCode(500, "An error occurred while creating the movie.");
+            }
+        }
+
+        [Authorize]
+        [HttpPut("UpdateReview/{id}")]
+        public async Task<IActionResult> UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (!await _movieRepository.DoesMovieExists(id))
+            {
+                return NotFound("an error occurred while updating the movie.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("An error occurred while updating the movie.");
+            }
+
+            try
+            {
+                var movie = await _movieRepository.GetMovieByIdAsync(id);
+                movie.Title = movieDto.Title;
+                movie.Description = movieDto.Description;
+                movie.Author = movieDto.Author;
+                movie.Genre = Enum.Parse<MovieGenre>(movieDto.Genre); // parse the value to convert them to appropriate types
+                movie.DateOfRelease = DateTime.ParseExact(movieDto.DateOfRelease, "dd-MM-yyyy", CultureInfo.InvariantCulture); // parse the value to convert them to appropriate types
+
+                var isMovieUpdated = await _movieRepository.UpdateMovieAsync(movie);
+
+                if (!isMovieUpdated)
+                {
+                    _logger.LogError($"An error occurred during updating data");
+                    return StatusCode(500, "An error occurred while updating the movie.");
+                }
+                return Ok(movie);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred during updating data: {ex.Message}");
+                return StatusCode(500, "An error occurred while updating the movie.");
+            }
+        }
+
+        [HttpGet("FilteredReviews")]
+        public async Task<IActionResult> FilterMovies(string str)
+        {
+            try
+            {
+                var allMovies = await _movieRepository.GetAllMoviesAsync();
+                var filteredMovies = allMovies
+                .Where(i => i.Title.ToLower().Contains(str.ToLower()) ||
+                    i.Description.ToLower().Contains(str.ToLower()) ||
+                    Enum.GetName(typeof(MovieGenre), i.Genre)?.ToLower().Contains(str.ToLower()) == true ||
+                    i.Author.ToLower().Contains(str.ToLower()))
+                .ToList();
+
+                var filteredmoviesDto = new List<MovieDto>();
+
+                foreach (Movie filteredMovie in filteredMovies)
+                {
+                    var movieDto = ConvertToMovieDto(filteredMovie);
+                    filteredmoviesDto.Add(movieDto);
+                }
+
+                return Ok(filteredmoviesDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred during retrieving data: {ex.Message}");
+                return StatusCode(500, "An error occurred while retrieving movies.");
+            }
+        }
+
+        [HttpDelete("DeleteMovie")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+
+            if (!await _movieRepository.DoesMovieExists(id)) 
+            {
+                return NotFound("An error occurred while deleting the movie.");
+            }
+
+            try
+            {
+                var isMovieDeleted = await _movieRepository.DeleteMovieAsync(id);
+
+                if (!isMovieDeleted)
+                {
+                    _logger.LogError($"An error occurred during deleting data");
+                    return StatusCode(500, "An error occurred while deleting the movie.");
+                }
+
+                return Ok("Movie successfully deleted");
+
+            } catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred during deleting data: {ex.Message}");
+                return StatusCode(500, "An error occurred while deleting the movie.");
             }
         }
 
--- Movies/Repository/MovieRepository.cs
+++ Movies/Repository/MovieRepository.cs
@@ -16,10 +16,10 @@ namespace Movies.Repository
 
         public async Task<ICollection<Movie>> GetAllMoviesAsync()
         {
-            return await _context.Movies.ToArrayAsync();
+            return await _context.Movies.ToListAsync();
         }
 
-        public async Task<Movie> GetMoviesByIdAsync(int id)
+        public async Task<Movie> GetMovieByIdAsync(int id)
         {
             return await _context.Movies.FirstOrDefaultAsync(i => i.Id == id);
         }
@@ -29,15 +29,47 @@ namespace Movies.Repository
             return await _context.Movies.Where(i => i.UserId == id).ToListAsync();
         }
 
-        public async Task<bool> CreateMovieAsync(Movie movie, User user)
+        public async Task<bool> CreateMovieAsync(Movie movie)
         {
-                movie.UserId = user.Id;
-                movie.User = user;
 
-                _context.Movies.Add(movie);
-                await _context.SaveChangesAsync();
-
-                return true;
+            _context.Movies.Add(movie);
+            return Save();
         }
+
+        public async Task<bool> UpdateMovieAsync(Movie movie)
+        {
+            _context.Update(movie);
+            _context.SaveChanges();
+            return Save();
+        }
+
+        public async Task<bool> RemoveMovieAsync(int id)
+        {
+            var movie = _context.Movies.FirstOrDefaultAsync(i => i.Id == id);
+            _context.Remove(movie);
+            _context.SaveChanges();
+            return Save();
+        }
+
+
+        public async Task<bool> DoesMovieExists(int id)
+        {
+            var movie = await _context.Movies.FirstOrDefaultAsync(i => i.Id == id);
+            if (movie == null)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+
     }
 }
